8c3f703c1831a7d0dbf7115943b10e2f70f2841b
Carolina559 <carolina.makoski.alves@escola.pr.gov.br>
2025-08-11 09:05:33 -0300

    jjj

---
+++ perguntas.js
@@ -0,0 +1,37 @@
+criaCartao(
+    'Bíblia',
+    'Quem abriu o mar vermelho?',
+    'Moisés'
+)
+
+criaCartao(
+    'filme',
+    'Que filme tem o nome de estrelas?',
+    'A culpa é das estrelas'
+)
+
+criaCartao(
+    'desenho',
+    'Que restaurante é em uma armadilha de siri?',
+    'siri cascudo'
+)
+
+criaCartao(
+    'animais',
+    'Que animal tem uma bolsa?',
+    'o canguru'
+)
+
+criaCartao(
+    'peixe',
+    'que peixe coloca mais de mil filhos?',
+    'o cavalo marinho'
+)
+
+criaCartao(
+    'planta',
+    'que flor que segue o sol?',
+    'o girassol'
+)
+
+
